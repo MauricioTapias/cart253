fc6d4cd6bd97c2fe50dfed32858b9aab158d4995
MauricioTapias <70979389+MauricioTapias@users.noreply.github.com>
2020-09-24 20:14:19 -0400

    E1: added mouse interactivity & changed the colors
    added mouseX and mouseY related movement for the shapes as well changed their colors. I also changed the colors of the background and made them change with the movement of mouseY.

--- Exercises/exercise1/js/script.js
+++ Exercises/exercise1/js/script.js
@@ -5,26 +5,26 @@ Pippin Barr
 Here is a description of this template p5 project.
 **************************************************/
 let bg = {
-  r: 0,
-  g: 0,
+  r: 250,
+  g: 100,
   b:0
 };
 let circle1 = {
-  x: 0,
+  x: 250,
   y: 250,
   size: 0,
   growthRate: 0.05,
-  speed: 0.25,
+  speed: 0.5,
   fill: 255,
   alpha: 255
 };
 let square1 = {
-  x: 500,
-  y: 150,
+  x: 250,
+  y: 250,
   size: 0,
   sizeRatio: 0.5,
   speed: -0.25,
-  fill: 255,
+  fill: 0,
   alpha: 255
 };
 
@@ -34,10 +34,7 @@ let square1 = {
 function setup() {
   createCanvas(500,500);
   noStroke();
-  // circle1.y = height/2;
-  // circle2.x = width;
-  // circle2.y = hright/2;
-
+rectMode(CENTER)
 }
 
 // draw()
@@ -45,25 +42,32 @@ function setup() {
 // Description of draw() goes here.
 function draw() {
    background(bg.r,bg.g,bg.b);
-   bg.b = map(circle1.x, 100, width, 0,255);
+   bg.b = map(mouseY, 0, width, 0,255);
+
+   //horizon line
+   line(0,250,500,250)
+   stroke(255)
 
    //triangle
    fill(255,255);
    triangle(0, 500, 500, 500, 250, 250);
 
+   //moving square (direction: left)
+   square1.y = square1.y + square1.speed;
+   square1.size = circle1.size * square1.sizeRatio;
+   square1.y = constrain(mouseY, 0, width/2)
+   fill(square1.fill, square1.alpha);
+   square(square1.x, square1.y, square1.size)
+
    //moving circle (direction: right)
-   circle1.x = circle1.x + circle1.speed;
+   circle1.x = mouseX;
    // circle1.x = constrain(circle1.x, 0, width/2)
    circle1.size = circle1.size + circle1.growthRate;
    circle1.size = constrain(circle1.size, 0, width)
    fill(circle1.fill, circle1.alpha);
    ellipse(circle1.x, circle1.y, circle1.size)
 
-   //moving square (direction: left)
-   square1.x = square1.x + square1.speed;
-   square1.size = circle1.size * square1.sizeRatio;
-   fill(square1.fill, square1.alpha);
-   square(square1.x, square1.y, square1.size)
+
 
 
 
