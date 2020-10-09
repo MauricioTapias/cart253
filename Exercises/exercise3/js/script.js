b51c9fd2ca59a5dc26b8cfb6e6a39924fabbe09c
MauricioTapias <70979389+MauricioTapias@users.noreply.github.com>
2020-10-09 01:12:00 -0400

    added circle 2 + random motion
    Displayed circle2 and made it run off in random directions.

--- Exercises/exercise3/js/script.js
+++ Exercises/exercise3/js/script.js
@@ -10,13 +10,36 @@ let circle1 = {
   size: 100,
   vx: 0,
   vy: 0,
-  speed: 5,
+  speed: 3,
 };
+
+let circle2 ={
+  x: 0,
+  y:0,
+  size: 100,
+  vx:0,
+  vy:0,
+  speed: 4,
+};
+
+let state = 'title';
 
 // setup()
 function setup() {
   createCanvas(500,500);
+  setupCircles();
 }
+
+function setupCircles() {
+  // Position circles separated from one another
+  circle2.x = width/3;
+
+  //start circles moving in a random direction
+  circle2.vx = random(-circle2.speed, circle2.speed);
+  circle2.vy = random(-circle2.speed, circle2.speed);
+}
+
+
 
 
 // draw()
@@ -28,6 +51,57 @@ function draw() {
   display();
 }
 
+//TITLES
+if (state === 'title') {
+  title();
+}
+  else if (state === 'simulation') {
+    simulation();
+  }
+  else if (state === 'love') {
+    love();
+  }
+  else if (state === 'sadness') {
+    sadness();
+  }
+
+
+function title() {
+  push();
+  textSize(64);
+  fill(200,100,100);
+  textAlign(CENTER, CENTER);
+  text('LOVE?', width/2, height/2);
+  pop();
+}
+
+
+simulation();
+
+function simulation() {
+  move();
+  checkOffscreen();
+  checkOverlap();
+  display();
+}
+
+function love() {
+  push();
+  textSize(64);
+  fill(255,150,150);
+  textAlign(CENTER, CENTER);
+  text('LOVE', width/2, height/2);
+  pop();
+}
+
+function sadness() {
+  push();
+  textSize(64);
+  fill(150,150,255);
+  textAlign(CENTER, CENTER);
+  text('RIP', width/2, height/2);
+  pop();
+}
 
 //CONTROLS FOR CIRCLE1
 function handleInput() {
@@ -52,11 +126,49 @@ function handleInput() {
   }
 }
 
+//MOVEMENT
 function move() {
   circle1.x = circle1.x + circle1.vx;
   circle1.y = circle1.y + circle1.vy;
+
+  circle2.x = circle2.x + circle2.vx;
+  circle2.y = circle2.y + circle2.vy;
 }
 
+//WHAT HAPPENS WHEN CIRCLES TOUCH
+function checkOffscreen() {
+  //check if the circles have gone offscreen
+  if (isOffscreen(circle2)) {
+    state = 'sadness';
+  }
+}
+
+function isOffscreen(circle2) {
+  if (circle2.x < 0 || circle2.x > width || circle2.y < 0 || circle2.y > height){
+  return true;
+}
+else {
+    return false;
+  }
+}
+
+function checkOverlap() {
+  //check if the circles overlap
+  let d = dist(circle1.x, circle1.y, circle2.x, circle2.y);
+  if (d < circle1.size/2 + circle2.size/2) {
+    state = 'love';
+  }
+}
+
+
+//DISPLAY
 function display() {
   ellipse(circle1.x, circle1.y, circle1.size);
+  ellipse(circle2.x, circle2.y, circle2.size);
+}
+
+function mousePressed() {
+  if (state === 'title') {
+    state = 'simulation';
+  }
 }
