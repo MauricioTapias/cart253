1807197098f8433ad48a5bab41f5f6a3a4764400
MauricioTapias <70979389+MauricioTapias@users.noreply.github.com>
2020-10-08 20:56:01 -0400

    started activity 05

---
+++ Activities/05_looking_love/js/script.js
@@ -0,0 +1,40 @@
+/**************************************************
+Template p5 project
+Pippin Barr
+
+Here is a description of this template p5 project.
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+  createCanvas(500, 500);
+background(255,75,40);
+
+//body
+fill(200)
+ellipse(250,500,270,220);
+
+
+//mouth
+rectMode(CENTER);
+rect(250,340,50,20);
+rect(250,400,30,40);
+
+//head
+ellipse(250,220,280,350);
+
+//eyes
+fill(0)
+ellipse(190,190,100,190);
+ellipse(315,190,100,190);
+}
+
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
