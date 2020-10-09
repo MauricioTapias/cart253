6c2ae6f2bb7ddd88d1ff26b6a02f28a5e3b2274b
MauricioTapias <70979389+MauricioTapias@users.noreply.github.com>
2020-10-09 00:47:57 -0400

    E3: addition of circle1 + controls
    displayed a white circle at the center of the canvas. the user can move it through the use of the arrow keys.

--- Exercises/exercise3/js/script.js
+++ Exercises/exercise3/js/script.js
@@ -4,17 +4,59 @@ Pippin Barr
 
 Here is a description of this template p5 project.
 **************************************************/
+let circle1 = {
+  x: 250,
+  y: 250,
+  size: 100,
+  vx: 0,
+  vy: 0,
+  speed: 5,
+};
 
 // setup()
-//
-// Description of setup() goes here.
 function setup() {
-
+  createCanvas(500,500);
 }
 
-// draw()
-//
-// Description of draw() goes here.
-function draw() {
 
+// draw()
+function draw() {
+  background(0);
+
+  handleInput();
+  move();
+  display();
+}
+
+
+//CONTROLS FOR CIRCLE1
+function handleInput() {
+  if (keyIsDown(UP_ARROW)) {
+    circle1.vy = -circle1.speed;
+  }
+  else if (keyIsDown(DOWN_ARROW)) {
+    circle1.vy = circle1.speed;
+  }
+  else {
+    circle1.vy = 0;
+  }
+
+  if (keyIsDown(RIGHT_ARROW)) {
+    circle1.vx = circle1.speed;
+  }
+  else if (keyIsDown(LEFT_ARROW)) {
+    circle1.vx = -circle1.speed;
+  }
+  else {
+    circle1.vx = 0;
+  }
+}
+
+function move() {
+  circle1.x = circle1.x + circle1.vx;
+  circle1.y = circle1.y + circle1.vy;
+}
+
+function display() {
+  ellipse(circle1.x, circle1.y, circle1.size);
 }
