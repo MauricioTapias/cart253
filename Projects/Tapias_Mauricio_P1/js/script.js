f693a0f10ea86a5c11e6062f7be6d6816544c225
MauricioTapias <70979389+MauricioTapias@users.noreply.github.com>
2020-10-14 19:46:58 -0400

    P1: transfered code from exercise 2
    Copy pasted code from exercise to in order to refer to it and modify it.

--- Projects/Tapias_Mauricio_P1/js/script.js
+++ Projects/Tapias_Mauricio_P1/js/script.js
@@ -6,17 +6,140 @@ Pippin Barr
 
 Here is a description of this template p5 project.
 **************************************************/
+let covid19 = {
+  x: 0,
+  y: 250,
+  size:100,
+  vx: 0,
+  vy:0,
+  speed: 5,
+  fill: {
+    r: 255,
+    g:50,
+    b:120,
+  }
+};
+
+let covid20 = {
+  x: 750,
+  y: 350,
+  size:100,
+  vx: 0,
+  vy:0,
+  speed: 5,
+  fill: {
+    r: 255,
+    g:50,
+    b:120,
+  }
+};
+
+let user = {
+  x: 250,
+  y:250,
+  size: 100,
+  fill: 255,
+};
+
+let kermitImage;
+
+function preload() {
+kermitImage = loadImage("assets/images/kermit.png")
+}
+
 
 // setup()
 //
-// Description of setup() goes here.
+// Canvas; covid19 positions; no cursor;
 function setup() {
+createCanvas(windowWidth,windowHeight);
 
+covid19.y = random(0,height);
+covid19.vx = covid19.speed;
+
+noCursor();
 }
 
 // draw()
 //
-// Description of draw() goes here.
+// background; no stroke; kermitimage; covid 19-20 movement & growth; user movement; display of user and covid 19-20;
 function draw() {
+   background (50, 255, 50);
 
+//shape outline
+   noStroke()
+
+//kermit image
+   imageMode(CENTER);
+   image(kermitImage, mouseX, mouseY);
+
+
+   // covid 19 movement
+   covid19.x = covid19.x + covid19.vx;
+   covid19.y = covid19.y + covid19.vy;
+
+   if (covid19.x > width) {
+     covid19.x = 0;
+     covid19.y = random(0,height);
+     }
+
+     //covid 19 growth
+    if (covid19.x < width/1.002){
+      covid19.size = covid19.size + 3;
+      }
+      else {
+        covid19.size = 100;
+      }
+
+      // covid 20 movement
+      covid20.x = covid20.x + covid20.vx;
+      covid20.y = covid20.y + covid20.vy;
+
+      if (covid20.x > width) {
+        covid20.x = 0;
+        covid20.y = random(0,height);
+        }
+
+        //covid 20 growth
+       if (covid20.x < width/1.002){
+         covid20.size = covid20.size + 1;
+         }
+         else {
+           covid20.size = 100;
+         }
+
+     //user movement
+     user.x = mouseX;
+     user.y = mouseY;
+
+     // check for bumping covid19
+     let d = dist(user.x,user.y,covid19.x,covid19.y);
+     if (d < covid19.size/2 + user.size/2) {
+    noLoop();
+     }
+
+     // display covid 19
+   fill(covid19.fill.r, covid19.fill.g, covid19.fill.b);
+   ellipse(covid19.x, covid19.y, covid19.size);
+
+   // display covid 20
+   fill(covid20.fill.r, covid20.fill.g, covid20.fill.b);
+   ellipse(covid20.x, covid20.y, covid20.size);
+
+   // display user
+   fill(user.fill);
+   square(user.x, user.y, user.size);
+
+}
+// Mouse functions: pressed & dragged;
+
+// covid 20 position change
+function mousePressed() {
+  covid20.x = mouseX;
+  covid20.y = mouseY;
+}
+
+// user cursor shrink
+function mouseDragged() {
+  user.size = user.size - 1;
 }
