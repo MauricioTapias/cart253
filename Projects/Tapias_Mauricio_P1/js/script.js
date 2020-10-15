5dc34935c4aef6b0611ea6099ee70b07467f594f
MauricioTapias <70979389+MauricioTapias@users.noreply.github.com>
2020-10-14 22:59:35 -0400

    P1: Made stickyImage move via the arrow keys
    I changed the movement of the stickyImage from the user's mouse input to the user's arrow keys.

--- Projects/Tapias_Mauricio_P1/js/script.js
+++ Projects/Tapias_Mauricio_P1/js/script.js
@@ -23,23 +23,18 @@ let outfit1 = {
 let covid20 = {
   x: 750,
   y: 350,
-  size:100,
+  size: 100,
   vx: 0,
   vy:0,
   speed: 5,
-  fill: {
-    r: 255,
-    g:50,
-    b:120,
-  }
 };
 
-let user = {
-  x: 250,
-  y:250,
-  size: 100,
-  fill: 255,
-};
+// let user = {
+//   x: 250,
+//   y:250,
+//   size: 100,
+//   fill: 255,
+// };
 
 let stickyImage;
 let outfit1Image;
@@ -67,9 +62,12 @@ noCursor();
 function draw() {
    background (255, 255, 255);
 
+handleInput();
+move();
+
 //Sticky image
    imageMode(CENTER);
-   image(stickyImage, mouseX, mouseY);
+   image(stickyImage, covid20.x, covid20.y);
 
 
    // covid 19 movement
@@ -81,18 +79,24 @@ function draw() {
      outfit1.x = random(0,width);
      }
 
+function move() {
+  covid20.x = covid20.x + covid20.vx;
+  covid20.y = covid20.y + covid20.vy;
+}
+
+
 
      //user movement
-     user.x = mouseX;
-     user.y = mouseY;
+     // user.x = mouseX;
+     // user.y = mouseY;
 
      // check for bumping covid19
-     let d = dist(user.x,user.y,outfit1.x,outfit1.y);
-     if (d < outfit1.size/2 + user.size/2) {
+     let d = dist(covid20.x,covid20.y,outfit1.x,outfit1.y);
+     if (d < outfit1.size/2 + covid20.size/2) {
     noLoop();
      }
 
-     // display covid 19
+     // display outfit1
    image(outfit1Image, outfit1.x, outfit1.y, outfit1.size);
 
 
@@ -100,26 +104,36 @@ function draw() {
 // Mouse functions: pressed & dragged;
 
 // covid 20 position change
-function mousePressed() {
-  covid20.x = mouseX;
-  covid20.y = mouseY;
-}
+// function mousePressed() {
+//   covid20.x = mouseX;
+//   covid20.y = mouseY;
+// }
+//
+// // user cursor shrink
+// function mouseDragged() {
+//   user.size = user.size - 1;
+// }
 
-// user cursor shrink
-function mouseDragged() {
-  user.size = user.size - 1;
-}
 
-
-function keyPressed() {
-  if (keycode === UP_ARROW) {
-    covid19.y = y - 10;
-  } else if (keycode === DOWN_ARROW) {
-    covid19.y = y + 10;
+function handleInput() {
+  if (keyIsDown (UP_ARROW)) {
+    covid20.vy = -covid20.speed;
   }
-  if (keycode === LEFT_ARROW) {
-    covid19.x = x - 5;
-  } else if (keycode === RIGHT_ARROW) {
-    covid19.x = x + 5;
+  else if (keyIsDown (DOWN_ARROW)) {
+    covid20.vy = covid20.speed;
+  }
+  else {
+    covid20.vy = 0;
+  }
+
+
+  if (keyIsDown (LEFT_ARROW)) {
+    covid20.vx = -covid20.speed;
+  }
+  else if (keyIsDown (RIGHT_ARROW)) {
+    covid20.vx = covid20.speed;
+  }
+  else {
+    covid20.vx = 0;
   }
 }
