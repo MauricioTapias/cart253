56bd5cc26600385a92afb81defcb62a6767eeaf7
MauricioTapias <70979389+MauricioTapias@users.noreply.github.com>
2020-10-22 17:02:25 -0400

    E4: Started exercise 4

--- Activities/05_looking_love/js/script.js
+++ Activities/05_looking_love/js/script.js
@@ -4,37 +4,144 @@ Pippin Barr
 
 Here is a description of this template p5 project.
 **************************************************/
+let circle1 ={
+  x: 0,
+  y:0,
+  size: 100,
+  vx:0,
+  vy:0,
+  speed: 3,
+};
+
+let circle2 ={
+  x: 350,
+  y: 250,
+  size: 100,
+  vx:0,
+  vy:0,
+  speed: 3,
+};
+
+let state = 'title'; // can be title, simulation, love, sadness
 
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
-  createCanvas(500, 500);
-background(255,75,40);
-
-//body
-fill(200)
-ellipse(250,500,270,220);
-
-
-//mouth
-rectMode(CENTER);
-rect(250,340,50,20);
-rect(250,400,30,40);
-
-//head
-ellipse(250,220,280,350);
-
-//eyes
-fill(0)
-ellipse(190,190,100,190);
-ellipse(315,190,100,190);
+  createCanvas (500,500);
+  setupCircles();
 }
 
+function setupCircles() {
+  // Position circles separated from one another
+  circle1.x = width/3;
+  circle2.x = 2 * width/3;
 
+  //start circles moving in a random direction
+  circle1.vx = random(-circle1.speed, circle1.speed);
+  circle2.vx = random(-circle2.speed, circle2.speed);
+  circle1.vy = random(-circle1.speed, circle1.speed);
+  circle2.vy = random(-circle2.speed, circle2.speed);
+}
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
+background (0);
 
+if (state === 'title') {
+  title();
+}
+  else if (state === 'simulation') {
+    simulation();
+  }
+  else if (state === 'love') {
+    love();
+  }
+  else if (state === 'sadness') {
+    sadness();
+  }
+}
+
+push();
+function title() {
+
+  textSize(64);
+  fill(200,100,100);
+  textAlign(CENTER, CENTER);
+  text('LOVE?', width/2, height/2);
+  pop();
+}
+
+
+simulation();
+
+function simulation() {
+  move();
+  checkOffscreen();
+  checkOverlap();
+  display();
+}
+
+function love() {
+  push();
+  textSize(64);
+  fill(255,150,150);
+  textAlign(CENTER, CENTER);
+  text('LOVE', width/2, height/2);
+  pop();
+}
+
+function sadness() {
+  push();
+  textSize(64);
+  fill(150,150,255);
+  textAlign(CENTER, CENTER);
+  text('RIP', width/2, height/2);
+  pop();
+}
+
+function move() {
+  //Move the circles
+  circle1.x = circle1.x + circle1.vx;
+  circle1.y = circle1.y + circle1.vy;
+
+  circle2.x = circle2.x + circle2.vx;
+  circle2.y = circle2.y + circle2.vy;
+}
+
+function checkOffscreen() {
+  //check if the circles have gone offscreen
+  if (isOffscreen(circle1) || isOffscreen(circle2)) {
+    state = 'sadness';
+  }
+}
+
+function isOffscreen(circle) {
+  if (circle.x < 0 || circle.x > width || circle.y < 0 || circle.y > height){
+  return true;
+}
+else {
+    return false;
+  }
+}
+
+function checkOverlap() {
+  //check if the circles overlap
+  let d = dist(circle1.x, circle1.y, circle2.x, circle2.y);
+  if (d < circle1.size/2 + circle2.size/2) {
+    state = 'love';
+  }
+}
+
+function display() {
+  //display the circles
+  ellipse(circle1.x, circle1.y, circle1.size);
+  ellipse(circle2.x, circle2.y, circle2.size);
+}
+
+function mousePressed() {
+  if (state === 'title') {
+    state = 'simulation';
+  }
 }
