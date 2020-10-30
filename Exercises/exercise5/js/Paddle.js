1bb865e5aba99f821eefc407779d195279af1778
MauricioTapias <70979389+MauricioTapias@users.noreply.github.com>
2020-10-29 22:19:08 -0400

    E5: Keypad paddle
    added keypad controls to the paddle.

--- Exercises/exercise5/js/Paddle.js
+++ Exercises/exercise5/js/Paddle.js
@@ -1,15 +1,29 @@
 class Paddle {
 
+  move() {
+    function handleInput() {
+
+      if (keyIsDown(LEFT_ARROW)) {
+        this.vx = -this.speed;
+      } else if (keyIsDown(RIGHT_ARROW)) {
+        this.vx = this.speed;
+      } else {
+        this.vx = 0;
+      }
+    }
+  }
+
+
 constructor(w,h) {
  this.width = w;
  this.height = h;
- this.x = 0;
+ this.x = windowWidth/2;
  this.y = height - this.height/2;
+ this.vx = 0;
+ this.speed = 5;
+
 }
 
-move() {
-  this.x = mouseX;
-}
 
 display() {
   push();
